7c740f9f7172d9de1402f2661324a7e86ac8c62c
guettafa <irontitou5@gmail.com>
2024-12-13 23:17:31 -0500

    Renamed GetById to GetByEmail + Added a doc comment to explain why IUserRepository exist

--- src/PathPaver.Application/Repository/Entities/IUserRepository.cs
+++ src/PathPaver.Application/Repository/Entities/IUserRepository.cs
@@ -2,13 +2,18 @@ using PathPaver.Domain.Entities;
 
 namespace PathPaver.Application.Repository.Entities;
 
+/**
+ * 
+ * Here there's a IUserRepository because we can't access to the UserRepository Impl from the PathPaver.Application project
+ * 
+ * This is due to the Clean architecture blocking us to do assembly reference directly to the Persistence project, but we
+ * can create Interfaces for the repository in Application and then
+ * link the interface and its implementation together in the main solution file [ Program.cs ]
+ *
+ *
+ * This interface is used implemented by UserRepository.cs in PathPaver.Persistence/Entities/ 
+ */
 public interface IUserRepository : IBaseRepository<User>
 {
-    // Asynchronous operation that return a value
-    /*
-     * This is why am using Task<T>
-     *
-     * PS : Removed Task for now
-     */
-    User GetById(long id);
+    User GetByEmail(string email);
 }
--- src/PathPaver.Application/Repository/IBaseRepository.cs
+++ src/PathPaver.Application/Repository/IBaseRepository.cs
@@ -2,10 +2,11 @@ using PathPaver.Domain.Common;
 
 namespace PathPaver.Application.Repository;
 
+
 public interface IBaseRepository<T> where T : BaseEntity
 {
-    T Get(long id);
+    T Get(string name);
     void Create(T inst);
-    void Update(long id, T newInst);
-    void Delete(long id);
+    void Update(string name, T newInst);
+    void Delete(string name);
 }
