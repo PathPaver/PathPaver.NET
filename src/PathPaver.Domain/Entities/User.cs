ff29ac5b40eb68a3bfb05ef5bc42659ec1de8396
guettafa <irontitou5@gmail.com>
2024-12-13 23:21:15 -0500

    Minor changes in User.cs

--- src/PathPaver.Domain/Entities/User.cs
+++ src/PathPaver.Domain/Entities/User.cs
@@ -1,23 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using PathPaver.Domain.Common;
 
 namespace PathPaver.Domain.Entities;
 
-public sealed class User(string? username, string email, string password, string biography = "") : BaseEntity
+public sealed class User(string email, string username,  string password, string biography = "") : BaseEntity
 {
     #region Properties
     
-    public string Email { get; } = email;
-    public string Username { get; set; } = username ?? $"Anonymous";
-    public string Biography { get; set; } = biography;
+    public string Email { get; set; } = email;
+    public string Username { get; set; } = username;
     public string Password { get; set; } = password;
-    
+    public string Biography { get; set; } = biography;
     #endregion
     
     public string ShowInfo()
     {
         return $"{Username} - " +
-               $"{Biography} - " +
-               $"{Email} - ";
+               $"{Biography} - ";
     }
 }
