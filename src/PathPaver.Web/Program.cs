5987168a5e8edbbe2399c6f6c335abef05097e07
guettafa <irontitou5@gmail.com>
2024-12-14 15:54:14 -0500

    Configured Logger for Serilog + Added builder config for Security variables like PRIVATE_KEY

--- src/PathPaver.Web/Program.cs
+++ src/PathPaver.Web/Program.cs
@@ -8,10 +8,21 @@ using PathPaver.Application.Services.Entities;
 using PathPaver.ML;
 using PathPaver.Persistence;
 using PathPaver.Persistence.Repository.Entities;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region Services
+#region Configuration
+
+/*
+ * Serilog - Logging
+ * 
+ * It go look at the appsettings.json file to check Serilog Config
+ */
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("Logs/log.txt")
+    .MinimumLevel.Warning()
+    .CreateLogger();
 
 if (builder.Environment.IsDevelopment())
 {
@@ -19,17 +30,35 @@ if (builder.Environment.IsDevelopment())
 }
 
 // To handle UserSecrets content for atlas cluster in DbSettings.cs file
-builder.Configuration.GetSection("MongoCluster").Get<DbSettings>();
+builder.Configuration
+    .GetSection("MongoCluster")
+    .Get<DbSettings>();
+
+// To handle UserSecrets content for Security related things in AuthSettings.cs file
+builder.Configuration
+    .GetSection("Security")
+    .Get<AuthSettings>();
+
+#endregion
+
+#region Scoped Services
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<UserService>();
+
+#endregion
+
+#region Prediction Related
 
 // PredictionEnginePool AKA the model that will predict our rent prices
 builder.Services.AddPredictionEnginePool<ApartmentInput, ApartmentOutput>()
     .FromFile(modelName: "RentPricePredictor", filePath: "../PathPaver.ML/Model/TheSavedModelFile",
         watchForChanges: true);
 
+#endregion
+
+#region Auth Related
 
 // Default configuration for Authentication
 builder.Services.AddAuthentication(auth =>
@@ -50,21 +79,29 @@ builder.Services.AddAuthentication(auth =>
     };
 });
 
+#endregion
+
+#region Services
+
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
+builder.Services.AddSerilog();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 #endregion
 
 var app = builder.Build();
+
+#region Security
 
 /*
  * Security Middleware for authorizations
  *
  * - Authentication related stuff should go here
+ * 
+ * CORS + Authorization + Authentication Middleware
  */
-
-#region Security
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -86,6 +123,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging(); // Change default logging to Serilog
 app.UseHttpsRedirection();
 
 #endregion
