9a00a518fc479f801659668ee07100e0b9d77d23
guettafa <irontitou5@gmail.com>
2024-12-14 15:52:03 -0500

    Added some logging in UserController.cs

--- src/PathPaver.Web/Controllers/UserController.cs
+++ src/PathPaver.Web/Controllers/UserController.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using PathPaver.Application.Common.Exceptions.Entities;
 using PathPaver.Application.DTOs;
 using PathPaver.Application.Services.Entities;
-using PathPaver.Domain.Entities;
 
 namespace PathPaver.Web.Controllers;
 
 [ApiController]
 [Route("/api/v1/users")]
-public class UserController(UserService userService) : ControllerBase
+public class UserController(
+    UserService userService, ILogger<UserController> logger) : ControllerBase
 {
     [HttpGet("{email}")]
     public IActionResult GetByEmail(string email)
@@ -16,10 +16,14 @@ public class UserController(UserService userService) : ControllerBase
         try
         {
             var u = userService.GetByEmail(email);
+            
+            logger.LogWarning($"Information report has been retrieved for user {email}");
+            
             return Ok(new UserDto(u.Username, u.Biography));
         }
         catch (Exception e)
         {
+            logger.LogError($"Tried to Get user info of ${email} but failed");
             throw new UserNotFoundException(email, e.Message);
         }        
     }
