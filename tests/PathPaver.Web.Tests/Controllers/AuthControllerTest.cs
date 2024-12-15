d7f5b2867591acd07a783f86ff3e2450e729ba79
guettafa <irontitou5@gmail.com>
2024-12-15 17:57:53 -0500

    Added Controller folder + AuthControllerTest.cs

---
+++ tests/PathPaver.Web.Tests/Controllers/AuthControllerTest.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+using PathPaver.Application.Services.Auth;
+using PathPaver.Web.Controllers;
+
+namespace PathPaver.Web.Tests.Controllers;
+ 
+[TestFixture]
+public class AuthControllerTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        
+    }
+
+    [Test]
+    public void When_TokenIsGenerated_ReturnJWT()
+    {
+    }
+}
