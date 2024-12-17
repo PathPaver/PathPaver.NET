4687e22303edc71c6970dc73d4297e9064281332
JF195 <davidefuoco63@gmail.com>
2024-12-17 13:58:16 -0500

    save token in file through script

--- run-test-script.sh
+++ run-test-script.sh
@@ -1,2 +1,6 @@
-curl -X POST --json @./test-data/login.json http://localhost:5231/api/v1/auth/login > test-data/result.json
-# artillery run test-load.yaml
+echo "token" > test-data/temp
+curl -X POST --json @./test-data/login.json http://localhost:5231/api/v1/auth/login | \
+    jq -r ".token" >> test-data/temp
+sed -z 's/^\s*//; s/\s*$//' test-data/temp> test-data/token.csv
+rm test-data/temp
+artillery run test-load.yaml
