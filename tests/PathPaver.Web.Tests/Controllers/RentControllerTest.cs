b0d2056559e5da75e01d0ef1caaa2935ae12bd62
guettafa <irontitou5@gmail.com>
2024-12-18 22:18:29 -0500

    Added content to the SetUp method

--- tests/PathPaver.Web.Tests/Controllers/RentControllerTest.cs
+++ tests/PathPaver.Web.Tests/Controllers/RentControllerTest.cs
@@ -1,22 +1,35 @@
+using Microsoft.Extensions.ML;
+using Moq;
+using PathPaver.Application.Services.Entities;
+using PathPaver.ML;
+using PathPaver.Web.Controllers;
+
 namespace PathPaver.Web.Tests.Controllers;
 
 [TestFixture]
 public class RentControllerTest
 {
+    private RentController _rentController;
+    
     [SetUp]
     public void SetUp()
     {
-        
+        var mockedPredEnginePool = new Mock<PredictionEnginePool<ApartmentInput, ApartmentOutput>>();
+        var mockedRentPredService = new Mock<RentPredictionService>();
+
+        _rentController = new RentController(
+            mockedPredEnginePool.Object, 
+            mockedRentPredService.Object);
     }
 
     [TearDown]
     public void TearDown()
     {
-        
+        // destroying everything
     }
 
     [Test]
-    public void Test1()
+    public void PredictRentPrice_WhenReceivePredictionDto_ReturnPredictionOID()
     {
         
     }
