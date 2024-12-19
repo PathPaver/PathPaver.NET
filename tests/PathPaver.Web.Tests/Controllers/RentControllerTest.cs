296054c9a0cdf19918b861612d3d4c8d0cb275fb
guettafa <irontitou5@gmail.com>
2024-12-18 22:06:21 -0500

    Created RentControllerTest.cs

---
+++ tests/PathPaver.Web.Tests/Controllers/RentControllerTest.cs
@@ -0,0 +1,23 @@
+namespace PathPaver.Web.Tests.Controllers;
+
+[TestFixture]
+public class RentControllerTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+        
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        
+    }
+}
