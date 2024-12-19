254a9a77fcffcb01636caeeda7fb821be4d705cc
Abderrahmane Arslane Guettaf <2243765@collegemv.qc.ca>
2024-12-19 09:00:33 -0500

    Added GraphControllerTest.cs

---
+++ tests/PathPaver.Web.Tests/Controllers/GraphControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using PathPaver.Application.Repository.Entities;
+using PathPaver.Application.Services.Entities;
+using PathPaver.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathPaver.Web.Tests.Controllers;
+
+[TestFixture]
+public class GraphControllerTest
+{
+    private GraphController _graphController;
+
+    const string WorkingGraphName = "app_type";
+    const string NonWorkingGraphName= "app_type";
+
+    #region Setup and TearDown
+
+    [SetUp]
+    public void SetUp()
+    {
+        var mockedIGraphRepo = new Mock<IGraphRepository>();
+        mockedIGraphRepo.Setup(x => x.GetGraphByName(WorkingGraphName))
+            .Returns(new Graph());
+
+        var graphService = new GraphService(mockedIGraphRepo.Object);
+
+        _graphController = new GraphController(graphService);
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+        // some teardown
+    }
+
+    #endregion
+
+
+    [Test]
+    public void Test1()
+    {
+
+    }
+}
